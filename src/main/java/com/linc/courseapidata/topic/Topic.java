18a40df17fd4ada337c03d01befbd26626347c6b
Mukhammad Pardaboev <mukhammad.pardaboev@sap.com>
2023-03-04 19:21:53 +0500

    Spring Boot Quick Start 28 - Creating a Spring Data JPA Repository

--- src/main/java/com/linc/courseapidata/topic/Topic.java
+++ src/main/java/com/linc/courseapidata/topic/Topic.java
@@ -1,6 +1,11 @@
 package com.linc.courseapidata.topic;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Topic {
+  @Id
   private String id;
   private String name;
   private String description;
